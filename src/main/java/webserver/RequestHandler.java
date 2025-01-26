e5a4e9eba9ef266b4b54a8d5592b99aff56f8f26
leesoobeen <02ggang9@gmail.com>
2025-01-26 12:42:49 +0900

    feat: Ch.3 요구사항 2

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,11 +1,19 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,6 +21,9 @@ public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 
     private Socket connection;
+    private String path;
+    private List<String> parameter = new ArrayList<>();
+
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
@@ -24,8 +35,42 @@ public class RequestHandler extends Thread {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));
+            int count = 1;
+            while (true) {
+                String line = bufferedReader.readLine();
+                if (count == 1) {
+                    path = line.split(" ")[1];
+                }
+
+                System.out.println(line);
+                count++;
+                if (line.isEmpty()) {
+                    count = 1;
+                    break;
+                }
+            }
+
+            // ====
+
+            String[] parameters = this.path.split("\\?")[1].split("&");
+            for (int i = 0; i < parameters.length; i++) {
+                String value = parameters[i].split("=")[1];
+                parameter.add(value);
+            }
+
+            User user = new User(
+                parameter.get(0),
+                parameter.get(1),
+                parameter.get(2),
+                parameter.get(3)
+            );
+
+
+
+            //
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+            byte[] body = Files.readAllBytes(new File("./webapp" + path).toPath());
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
